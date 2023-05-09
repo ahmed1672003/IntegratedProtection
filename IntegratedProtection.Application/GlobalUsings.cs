7ff9d1e84d0bfed0196219c668d02eff5791b83b
Ahmed Adel Basha <ahmedadel1672003@gmail.com>
2023-05-09 20:49:38 +0300

    ApplayChanges

--- IntegratedProtection.Application/GlobalUsings.cs
+++ IntegratedProtection.Application/GlobalUsings.cs
@@ -9,10 +9,12 @@ global using IntegratedProtection.Infrastructure.IRepositories;
 global using MediatR;
 global using Microsoft.AspNetCore.Http;
 global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
 global using Microsoft.Extensions.DependencyInjection;
 global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
 global using System.Net;
 global using System.Reflection;
+
 
 namespace IntegratedProtection.Application;
