55b3277a3fadbcd43101394d0ab15c6173468352
Ahmed Adel Basha <ahmedadel1672003@gmail.com>
2023-05-12 19:07:59 +0300

    ApplayChanges

---
+++ IntegratedProtection.API/Controllers/ServicesController.cs
@@ -0,0 +1,9 @@
+﻿namespace IntegratedProtection.API.Controllers;
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class ServicesController : IntegratedProtectionController
+{
+    public ServicesController(IMediator mediator) : base(mediator) { }
+
+}
