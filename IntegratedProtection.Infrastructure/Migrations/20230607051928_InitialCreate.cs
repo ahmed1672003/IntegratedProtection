60d32e1113824b3a88cf4ebdd4605ca167f2b884
Ahmed Adel Basha <ahmedadel1672003@gmail.com>
2023-06-07 08:27:24 +0300

    ApplyChanges

---
+++ IntegratedProtection.Infrastructure/Migrations/20230607051928_InitialCreate.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IntegratedProtection.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "CivilRegistry");
+
+            migrationBuilder.EnsureSchema(
+                name: "Traffic");
+
+            migrationBuilder.EnsureSchema(
+                name: "CentralSecurity");
+
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                schema: "Traffic",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Model = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    OwnerSSN = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    Company = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Color = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Number = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    Letters = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Criminals",
+                schema: "CentralSecurity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SSN = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    CreatedData = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Criminals", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Drivers",
+                schema: "Traffic",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SSN = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drivers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                schema: "CivilRegistry",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SSN = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    Gender = table.Column<string>(type: "nvarchar(4)", maxLength: 4, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    MiddelName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    StreetNumber = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    StreetName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Center = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Governorate = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Religion = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TrafficOfficers",
+                schema: "Traffic",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Center = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    MiddelName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrafficOfficers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CarsDrivers",
+                schema: "Traffic",
+                columns: table => new
+                {
+                    CarId = table.Column<int>(type: "int", nullable: false),
+                    DriverId = table.Column<int>(type: "int", nullable: false),
+                    CreatedData = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarsDrivers", x => new { x.CarId, x.DriverId });
+                    table.ForeignKey(
+                        name: "FK_CarsDrivers_Cars_CarId",
+                        column: x => x.CarId,
+                        principalSchema: "Traffic",
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CarsDrivers_Drivers_DriverId",
+                        column: x => x.DriverId,
+                        principalSchema: "Traffic",
+                        principalTable: "Drivers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Cards",
+                schema: "CivilRegistry",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SSN = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: false),
+                    Job = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PersonId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Cards_Persons_PersonId",
+                        column: x => x.PersonId,
+                        principalSchema: "CivilRegistry",
+                        principalTable: "Persons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StolenCars",
+                schema: "Traffic",
+                columns: table => new
+                {
+                    CarId = table.Column<int>(type: "int", nullable: false),
+                    TrafficOfficerId = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StolenCars", x => new { x.CarId, x.TrafficOfficerId });
+                    table.ForeignKey(
+                        name: "FK_StolenCars_Cars_CarId",
+                        column: x => x.CarId,
+                        principalSchema: "Traffic",
+                        principalTable: "Cars",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_StolenCars_TrafficOfficers_TrafficOfficerId",
+                        column: x => x.TrafficOfficerId,
+                        principalSchema: "Traffic",
+                        principalTable: "TrafficOfficers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_PersonId",
+                schema: "CivilRegistry",
+                table: "Cards",
+                column: "PersonId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CarsDrivers_DriverId",
+                schema: "Traffic",
+                table: "CarsDrivers",
+                column: "DriverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_StolenCars_TrafficOfficerId",
+                schema: "Traffic",
+                table: "StolenCars",
+                column: "TrafficOfficerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cards",
+                schema: "CivilRegistry");
+
+            migrationBuilder.DropTable(
+                name: "CarsDrivers",
+                schema: "Traffic");
+
+            migrationBuilder.DropTable(
+                name: "Criminals",
+                schema: "CentralSecurity");
+
+            migrationBuilder.DropTable(
+                name: "StolenCars",
+                schema: "Traffic");
+
+            migrationBuilder.DropTable(
+                name: "Persons",
+                schema: "CivilRegistry");
+
+            migrationBuilder.DropTable(
+                name: "Drivers",
+                schema: "Traffic");
+
+            migrationBuilder.DropTable(
+                name: "Cars",
+                schema: "Traffic");
+
+            migrationBuilder.DropTable(
+                name: "TrafficOfficers",
+                schema: "Traffic");
+        }
+    }
+}
