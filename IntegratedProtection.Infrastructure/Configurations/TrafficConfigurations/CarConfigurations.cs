2239e0f37b1a07d14ef0693d10a35ee4a726f60d
Ahmed Adel Basha <ahmedadel1672003@gmail.com>
2023-06-19 20:09:02 +0300

    ApplyChanges

--- IntegratedProtection.Infrastructure/Configurations/TrafficConfigurations/CarConfigurations.cs
+++ IntegratedProtection.Infrastructure/Configurations/TrafficConfigurations/CarConfigurations.cs
@@ -4,5 +4,11 @@ public class CarConfigurations : IEntityTypeConfiguration<Car>
 {
     public void Configure(EntityTypeBuilder<Car> builder)
     {
+        builder.Property(p => p.Number)
+            .HasMaxLength(100);
+
+        builder.Property(c => c.Letters)
+            .HasMaxLength(100);
+
     }
 }
